8912c77f89833d660b2462276963ebf6b1f59bcf
hlina alemayoh <hilubecoder@gmail.com>
2025-07-27 21:04:16 +0300

    add some fixes

--- alx-project-0x03/components/layouts/Layout.tsx
+++ alx-project-0x03/components/layouts/Layout.tsx
@@ -1,8 +1,8 @@
-import {LayoutProps} from '@/interface';
+
 import { ReactNode } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
-
+import { LayoutProps } from '@/interface';
 
 
 const Layout: React.FC<LayoutProps> = ({ children }) => (
